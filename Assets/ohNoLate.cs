4d371f94615dd666af1cbfcf3235410eb5699132
amandaliyuan <aliyuanh@gmail.com>
2018-11-17 10:25:26 -0800

    added start scene
    stuff

---
+++ Assets/ohNoLate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ohNoLate : MonoBehaviour {
+    public float letterPause;
+    string message;
+    Text textComp;
+	// Use this for initialization
+	void Start () {
+        textComp = GetComponent<Text>();
+        message = textComp.text;
+        textComp.text = "";
+        StartCoroutine(TypeText());
+	}
+	IEnumerator TypeText()
+    {
+        foreach(char letter in message.ToCharArray())
+        {
+            textComp.text += letter;
+            yield return new WaitForSeconds(letterPause);
+        }
+    }
+
+}
