f8e79bab6c0c7ed9a1117fe63d256d06f02e5c60
amandaliyuan <aliyuanh@gmail.com>
2018-11-17 09:39:45 -0800

    Basics
    Basic stuff

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+    Rigidbody rb;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * speed);
+        }else if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.back * speed );
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * speed);
+        }else if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * speed);
+        }
+
+
+    }
+
+
+}
