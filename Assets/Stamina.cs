4b67930105ffb5a00dab4420615b0677b7f2c001
Nhi Van <nhi_van@s-169-232-236-123.resnet.ucla.edu>
2018-11-17 16:15:16 -0800

    added Stamina

---
+++ Assets/Stamina.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stamina : MonoBehaviour {
+
+	public GameObject player;
+	// Use this for initialization
+
+	public Slider Slider;
+
+	void Start () {
+		Slider = GetComponent<Slider>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Slider.value = player.GetComponent<playerMovement>().stamina/100;
+	}
+}
